e6edf72647a71b076de85ee343a86799b434bcdc
eulerianKnight <himanshu3.1415926@gmail.com>
2023-09-08 23:21:30 +0530

    modified app.py

--- app.py
+++ app.py
@@ -28,30 +28,35 @@ def load_embedding_model():
 
 st.title("PDF Question Answering")
 
-with st.sidebar:
-    pdf_file = st.file_uploader("**Upload PDF**", type="pdf")
 
-    if st.button("Submit") and pdf_file is not None:
-        with st.spinner(text="Uploading PDF and Generating Embeddings..."):
-            with NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
-                shutil.copyfileobj(pdf_file, tmp)
-                tmp_path = Path(tmp.name)
-                st.session_state['pdf_qa_model'].config = {
-                    "pdf_path": str(tmp_path), 
-                }
-                st.session_state['pdf_qa_model'].embedding = load_embedding_model()
-                st.session_state['pdf_qa_model'].llm = load_llm()
-                st.session_state['pdf_qa_model'].init_embeddings()
-                st.session_state['pdf_qa_model'].init_models()
-                st.session_state['pdf_qa_model'].vector_db_pdf()
-                st.sidebar.success("PDF uploaded successfully")
+def main():
+    with st.sidebar:
+        pdf_file = st.file_uploader("**Upload PDF**", type="pdf")
 
-question = st.text_input('Ask a question', 'What is this document?')
+        if st.button("Submit") and pdf_file is not None:
+            with st.spinner(text="Uploading PDF and Generating Embeddings..."):
+                with NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
+                    shutil.copyfileobj(pdf_file, tmp)
+                    tmp_path = Path(tmp.name)
+                    st.session_state['pdf_qa_model'].config = {
+                        "pdf_path": str(tmp_path), 
+                    }
+                    st.session_state['pdf_qa_model'].embedding = load_embedding_model()
+                    st.session_state['pdf_qa_model'].llm = load_llm()
+                    st.session_state['pdf_qa_model'].init_embeddings()
+                    st.session_state['pdf_qa_model'].init_models()
+                    st.session_state['pdf_qa_model'].vector_db_pdf()
+                    st.sidebar.success("PDF uploaded successfully")
 
-if st.button("Answer"):
-    try:
-        st.session_state['pdf_qa_model'].retrieval_qa_chain()
-        answer = st.session_state['pdf_qa_model'].answer_query(question)
-        st.write(f"{answer}")
-    except Exception as e:
-        st.error(f"Error answering the question: {str(e)}")
+    question = st.text_input('Ask a question', 'What is this document?')
+
+    if st.button("Answer"):
+        try:
+            st.session_state['pdf_qa_model'].retrieval_qa_chain()
+            answer = st.session_state['pdf_qa_model'].answer_query(question)
+            st.write(f"{answer}")
+        except Exception as e:
+            st.error(f"Error answering the question: {str(e)}")
+
+if __name__ == "__main__":
+    main()
